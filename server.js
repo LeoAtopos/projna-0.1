3d562b2d3539d210ddd3a9e30fecaf57ce0c13a5
LeoAtopos <275142760@qq.com>
2016-07-24 19:01:43 +0800

    1
    1

--- server.js
+++ server.js
@@ -1,13 +1,53 @@
 var http = require("http");
 var connect = require("connect");
+var fs = require("fs");
 
 var app = connect();
 
-function index(request, response){
+function index(request, response, next){
 	//send the index home page.
+	response.writeHead(200,{"Context-Type":"text/plain"});
+	response.write("hahah");
+	response.end();
+	next();
 }
 
-app.use('/',index);
+function homepage(request, response) {
 
-http.createServer(app).listen(1337, "127.0.0.1"); 
+    if( request.method == 'GET' ){
+        response.writeHead(200, {"Content-Type": "text/html"});
+        //Open file as readable stream, pipe stream to response object
+        fs.createReadStream(__dirname + "/views/index.html").pipe(response);
+    }else{
+        send404Response(response);
+    }
+
+}
+
+function send404Response(response){
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("Error 404 - Page not found");
+    response.end();
+}
+
+// app.use('/',index);
+app.use('/home',homepage);
+
+
+http.createServer(app).listen(1337); 
 console.log("Server running at http://127.0.0.1:1337/");
+
+
+
+//Handle their request
+function onRequest(request, response) {
+
+    if( request.method == 'GET' && request.url == '/' ){
+        response.writeHead(200, {"Content-Type": "text/html"});
+        //Open file as readable stream, pipe stream to response object
+        fs.createReadStream("./index.html").pipe(response);
+    }else{
+        send404Response(response);
+    }
+
+}
